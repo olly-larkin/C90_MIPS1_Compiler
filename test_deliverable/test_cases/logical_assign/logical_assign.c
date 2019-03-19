afdbe9ebbf4640e197c3ae8c0c3cf66e24c9c2a9
Darrekt <darrick.lau15@imperial.ac.uk>
2019-03-19 16:16:04 +0000

    +some tests & management

---
+++ test_deliverable/test_cases/logical_assign/logical_assign.c
@@ -0,0 +1,17 @@
+int func(){ //OR
+    int x = 0xA;
+    x|= 0x5;
+    return x;
+}
+
+int funk(){ //AND
+    int x = 0x0F;
+    x&=0;
+    return x;
+}
+
+int fonc(){ //XOR
+    int x = 0x0F;
+    x^=0xF0;
+    return x;
+}
