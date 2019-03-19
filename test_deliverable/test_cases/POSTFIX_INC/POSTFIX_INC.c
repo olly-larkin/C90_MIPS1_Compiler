7c806518e515629b3da6d6c4c449c2a71cc0e9a7
Olly Larkin <olly.larkin@icloud.com>
2019-03-19 14:08:27 +0000

    changed test

--- test_deliverable/test_cases/POSTFIX_INC/POSTFIX_INC.c
+++ test_deliverable/test_cases/POSTFIX_INC/POSTFIX_INC.c
@@ -1,11 +1,11 @@
 
-int func() {
-    int x = -1;
-    x++;
-    return x;
-}
-
-// int func2() {
-//     int x = 0;
-//     return x++;
+// int func() {
+//     int x = -1;
+//     x++;
+//     return x;
 // }
+
+int func2() {
+    int x = 0;
+    return x++;
+}
