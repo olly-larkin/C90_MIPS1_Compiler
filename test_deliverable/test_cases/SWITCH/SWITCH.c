fbf29fa8fb709013a9e421e57f9cb7048e5fc505
Olly Larkin <olly.larkin@icloud.com>
2019-03-20 10:51:12 +0000

    switch test

--- test_deliverable/test_cases/SWITCH/SWITCH.c
+++ test_deliverable/test_cases/SWITCH/SWITCH.c
@@ -1,13 +1,9 @@
 
-int func2(int num) {
+int func(int num) {
     switch(num) {
         case 1: return 1;
         case 2: return 2;
         case 3: return 3;
         default: return 4;
     }
-}
-
-int func() {
-    return func2(1) + func2(3) - func2(20);
 }
--- test_deliverable/test_cases/SWITCH/SWITCH_driver.c
+++ test_deliverable/test_cases/SWITCH/SWITCH_driver.c
@@ -1,6 +1,6 @@
 
-int func();
+int func(int num);
 
 int main() {
-    return func();
+    return func(1) + func(3) - func(20);
 }
