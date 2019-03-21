31ba38bf0ce182fa2ecd9e6f8a2dee4e06f35465
Darrick Lau <dl5215@eews305-017.ee.ic.ac.uk>
2019-03-21 18:49:57 +0000

    Z pointer

--- test_deliverable/test_cases/Z_pointers/Z_pointers.c
+++ test_deliverable/test_cases/Z_pointers/Z_pointers.c
@@ -1,7 +1,9 @@
-int imagination(){
-    return 0;
-}
-
-int* supplement(int array[2]){
-    return 0;
+void imagination(int* ptr[5]){   
+//subtracts 1 from each element using unnecessarily verbose pointer arithmetic and dereferencing
+    int i;
+    for(i=0; i<5; i++){
+        //*(*(ptr+i)) = *(*(ptr+i)) - 1; //doesn't seg fault if you comment this out
+        (*(*(ptr+i)))--;                //alternate version
+    }
+    return;
 }
