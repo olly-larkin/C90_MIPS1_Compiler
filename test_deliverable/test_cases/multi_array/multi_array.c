2038a0f60efcc01fdaccd978ce3bfd94341c6093
Olly Larkin <olly.larkin@icloud.com>
2019-03-25 22:15:24 +0000

    fixed test

--- test_deliverable/test_cases/multi_array/multi_array.c
+++ test_deliverable/test_cases/multi_array/multi_array.c
@@ -3,9 +3,10 @@ int x[2][3];
 
 int func() {
     int y[2][3];
+    int i, j;
 
-    for (int i = 0; i < 2; ++i) {
-        for (int j = 0; j < 3; ++j) {
+    for (i = 0; i < 2; ++i) {
+        for (j = 0; j < 3; ++j) {
             y[i][j] = i + j;
             x[i][j] = i - j;
         }
